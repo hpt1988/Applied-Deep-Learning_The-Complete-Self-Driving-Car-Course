d98d0441c06a56453bfdc124948dc17a42a7295a
EXTERNAL Pourtavakoli Hamzeh (ALTEN GmbH, CC-DA/EPV2) <external.Hamzeh.Pourtavakoli@de.bosch.com>
2020-04-18 19:53:08 +0200

    creating a mask for our region of interest

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -9,9 +9,19 @@ def canny(image):
     
     return canny
 
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([
+        [(200, height), (1100, height), (550, 250)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+
+    return mask
+
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 
-plt.imshow(canny)
-plt.show()
+cv2.imshow("result", region_of_interest(canny))
+cv2.waitKey(0)
