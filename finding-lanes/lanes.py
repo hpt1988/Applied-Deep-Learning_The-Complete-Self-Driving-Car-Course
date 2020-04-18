6738764d6af6e5fcc1996c6d9ced60a3fd40a4a7
EXTERNAL Pourtavakoli Hamzeh (ALTEN GmbH, CC-DA/EPV2) <external.Hamzeh.Pourtavakoli@de.bosch.com>
2020-04-18 19:21:07 +0200

    defining canny function and moving the canny fucntionality to it

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -1,11 +1,16 @@
 import cv2
 import numpy as np
 
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    
+    return canny
+
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
-canny = cv2.Canny(blur, 50, 150)
+canny = canny(lane_image)
 
 cv2.imshow('result', canny)
 cv2.waitKey(0)
