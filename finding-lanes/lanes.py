2a7d188759ff44288c9184bde9807878f7dad315
EXTERNAL Pourtavakoli Hamzeh (ALTEN GmbH, CC-DA/EPV2) <external.Hamzeh.Pourtavakoli@de.bosch.com>
2020-04-22 18:01:26 +0200

    loading video instead of image and detecting lane lines in each frame

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -56,14 +56,29 @@ def region_of_interest(image):
 
     return masked_image
 
-image = cv2.imread('test_image.jpg')
-lane_image = np.copy(image)
-canny_image = canny(lane_image)
-cropped_image = region_of_interest(canny_image)
-lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-averaged_lines = average_slope_intercept(lane_image, lines)
-line_image = display_line(lane_image, averaged_lines)
-combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+# image = cv2.imread('test_image.jpg')
+# lane_image = np.copy(image)
+# canny_image = canny(lane_image)
+# cropped_image = region_of_interest(canny_image)
+# lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+# averaged_lines = average_slope_intercept(lane_image, lines)
+# line_image = display_line(lane_image, averaged_lines)
+# combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 
-cv2.imshow("result", combo_image)
-cv2.waitKey(0)
+# cv2.imshow("result", combo_image)
+# cv2.waitKey(0)
+
+cap = cv2.VideoCapture("test2.mp4")
+while(cap.isOpened()):
+    _, frame = cap.read() #decodes every video frame and return two values. 1st one is a boolean, which we leave it as blank, and the 2nd one is the frame which is an image
+    
+    #copy of the above algorithm, only replacing the lange image with the current fram in the video
+    canny_image = canny(frame)
+    cropped_image = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_line(frame, averaged_lines)
+    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+
+    cv2.imshow("result", combo_image)
+    cv2.waitKey(1) #waiting 1 milisecond between frames
