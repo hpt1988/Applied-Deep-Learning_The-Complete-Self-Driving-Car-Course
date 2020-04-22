48e1d36f5ff16a2f10b63847f9b3e8b0f42c1ba2
EXTERNAL Pourtavakoli Hamzeh (ALTEN GmbH, CC-DA/EPV2) <external.Hamzeh.Pourtavakoli@de.bosch.com>
2020-04-22 16:38:49 +0200

    increasing the consistency in code

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -31,8 +31,8 @@ def region_of_interest(image):
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-canny = canny(lane_image)
-cropped_image = region_of_interest(canny)
+canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
 line_image = display_image(lane_image, lines)
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
